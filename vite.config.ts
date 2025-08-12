da7a71eba52050f5b75a568fbd3c0a78fd063d30
Mike <michaelgrishchenko@mac.lan>
2025-08-12 13:16:44 -0700

    updated json

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://mychordbook.netlify.app/' : '/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    copyPublicDir: true
   }
 })
